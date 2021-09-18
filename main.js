851c711d2f9547f290e867d35c1c5fe6571d2374
fernandacaperon <fernandacaperon@gmail.con>
2021-09-18 18:37:56 -0300

    javascript-Desafio 1

---
+++ main.js
@@ -0,0 +1,7 @@
+var nombre = prompt ("Completa tu nombre")
+
+var apellido = prompt ("Completa tu apellido")
+
+var nombreCompleto = nombre + " " + apellido ;
+
+alert ('Gracias' + ' ' + nombreCompleto)
